0a348171e9af07472e5d8ab22b508d0aee56ed6b
carohansen73 <laaflaka.8@hotmail.com>
2026-07-15 09:22:53 -0300

    Fix: Forzar URLs HTTPS en producción para solucionar problemas de contenido mixto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use App\View\Composers\EventosComposer;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forzar HTTPS en producción para evitar bloqueos CSP y Mixed Content
+        if (config('app.env') === 'production' || env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
         Paginator::useTailwind();
         View::composer('components.eventos-slider', EventosComposer::class);
     }
--- resources/views/layouts/app-public.blade.php
+++ resources/views/layouts/app-public.blade.php
@@ -26,7 +26,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
         <!-- Scripts -->
-        @vite(['resources/css/app-publico.css', 'resources/css/galery.css', 'resources/js/app.js'])
+        @vite(['resources/css/app-publico.css', 'resources/css/galery.css'])
         @stack('styles')
     </head>
     <body>
@@ -75,6 +75,7 @@
             </script>
         @endif --}}
 
+        @vite(['resources/js/app.js'])
          <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         @stack('scripts')
